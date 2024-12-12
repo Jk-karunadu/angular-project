f95e28ec294cbb59d7452d52f471cfff730b77b5
Jashwanth-rit <158207370+Jashwanth-rit@users.noreply.github.com>
2024-12-12 23:58:27 +0530

    added url

--- src/app/sellerservice/seller.service.ts
+++ src/app/sellerservice/seller.service.ts
@@ -3,151 +3,150 @@ import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { login, Product, SignUp } from '../datatype';
 import { Router } from '@angular/router';
+import { environment } from '../../../environment.prod';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SellerService {
-
-  isSellerLoggedIn = new BehaviorSubject<boolean>(false); 
-  isUserLoggedIn = new BehaviorSubject<boolean>(false); 
+  isSellerLoggedIn = new BehaviorSubject<boolean>(false);
+  isUserLoggedIn = new BehaviorSubject<boolean>(false);
   issellerLoginError = new EventEmitter<boolean>(false);
   isuserLoginError = new EventEmitter<boolean>(false);
 
-  constructor(private http: HttpClient, private router: Router) {}
-
   private cartLengthSubject = new BehaviorSubject<number>(0);
   cartLength$ = this.cartLengthSubject.asObservable();
+
+  constructor(private http: HttpClient, private router: Router) {}
 
   updateCartLength(length: number): void {
     this.cartLengthSubject.next(length);
   }
 
   sellerSignUp(data: SignUp): void {
-    console.warn("Service called");
-    this.http.post('http://localhost:3000/seller', data, { observe: 'response' })
+    this.http.post(`${environment.backendUrl}/seller`, data, { observe: 'response' })
       .subscribe((res) => {
-        localStorage.setItem("seller", JSON.stringify(res.body));
+        localStorage.setItem('seller', JSON.stringify(res.body));
         this.router.navigate(['seller-home']);
-        console.warn(res);
       });
   }
+
   userSignUp(data: SignUp): void {
-    console.warn("Service called");
-    this.http.post('http://localhost:3000/user', data, { observe: 'response' })
+    this.http.post(`${environment.backendUrl}/user`, data, { observe: 'response' })
       .subscribe((res) => {
-        localStorage.setItem("user", JSON.stringify(res.body));
+        localStorage.setItem('user', JSON.stringify(res.body));
         this.router.navigate(['']);
-        console.warn(res);
       });
   }
 
   addProduct(data: any): Observable<any> {
-    console.warn("Service called");
-    return this.http.post('http://localhost:3000/products', data, { observe: 'response' });
+    return this.http.post(`${environment.backendUrl}/products`, data, { observe: 'response' });
   }
 
   sellerLogin(data: login): void {
-    console.warn("Service called");
-    this.http.get(`http://localhost:3000/seller?email=${data.email}&password=${data.password}`, { observe: 'response' })
+    this.http.get(`${environment.backendUrl}/seller?email=${data.email}&password=${data.password}`, { observe: 'response' })
       .subscribe((res: any) => {
         if (res && res.body && res.body.length) {
-          localStorage.setItem("seller", JSON.stringify(res.body));
+          localStorage.setItem('seller', JSON.stringify(res.body));
           this.router.navigate(['seller-home']);
-          console.warn(res);
         } else {
-          alert("Login failed, incorrect email or password.");
+          alert('Login failed, incorrect email or password.');
           this.issellerLoginError.emit(true);
         }
       });
   }
+
   userLogin(data: login): void {
-    console.warn("Service called");
-    this.http.get(`http://localhost:3000/user?email=${data.email}&password=${data.password}`, { observe: 'response' })
+    this.http.get(`${environment.backendUrl}/user?email=${data.email}&password=${data.password}`, { observe: 'response' })
       .subscribe((res: any) => {
         if (res && res.body && res.body.length) {
-          localStorage.setItem("user", JSON.stringify(res.body));
+          localStorage.setItem('user', JSON.stringify(res.body));
           this.router.navigate(['']);
-          console.warn(res);
         } else {
-          alert("Login failed, incorrect email or password.");
+          alert('Login failed, incorrect email or password.');
           this.isuserLoginError.emit(true);
         }
       });
   }
 
-  getproducts(){
-    return this.http.get('http://localhost:3000/products');
+  getproducts(): Observable<any> {
+    return this.http.get(`${environment.backendUrl}/products`);
   }
-  getbuy(){
-    return this.http.get('http://localhost:3000/buy');
+
+  getbuy(): Observable<any> {
+    return this.http.get(`${environment.backendUrl}/buy`);
   }
 
   searchprod(query: string): Observable<Product[]> {
-    return this.http.get<Product[]>(`http://localhost:3000/products?q=${query}`);
-  }
-  getcart(){
-    return this.http.get('http://localhost:3000/cart');
-  }
-  getproductshome(){
-
-    return this.http.get(`http://localhost:3000/products`);
-    // return this.http.get(`http://localhost:3000/carousel?_limit=4`);
-  }
-  getorders(){
-
-    return this.http.get(`http://localhost:3000/order`);
-    // return this.http.get(`http://localhost:3000/carousel?_limit=4`);
-  }
-  getslider(){
-    return this.http.get(`http://localhost:3000/carousel?_limit=4`);
-  }
-  delproduct(data:any){
-    return this.http.delete(`http://localhost:3000/products/${data}`)
-  }
-  delcart(data:any){
-    return this.http.delete(`http://localhost:3000/cart/${data}`)
-  }
-  delorders(data:any){
-    return this.http.delete(`http://localhost:3000/order/${data}`)
-  }
-  delbuy(data:any){
-    return this.http.delete(`http://localhost:3000/buy/${data}`)
-  }
-  getupdate(id:any){
-    return this.http.get(`http://localhost:3000/products?id=${id}`)
-
-  }
-  addtocart(product:any){
-    return this.http.post('http://localhost:3000/cart', product);
-  }
-  addtobuy(product:any){
-    return this.http.post('http://localhost:3000/buy', product);
-  }
-  orderdetails(product:any){
-    return this.http.post('http://localhost:3000/order', product);
+    return this.http.get<Product[]>(`${environment.backendUrl}/products?q=${query}`);
   }
 
-  updateproduct(data:any,id:any){
+  getcart(): Observable<any> {
+    return this.http.get(`${environment.backendUrl}/cart`);
+  }
 
-return this.http.put(`http://localhost:3000/products/${id}`,data);
+  getproductshome(): Observable<any> {
+    return this.http.get(`${environment.backendUrl}/products`);
+  }
+
+  getorders(): Observable<any> {
+    return this.http.get(`${environment.backendUrl}/order`);
+  }
+
+  getslider(): Observable<any> {
+    return this.http.get(`${environment.backendUrl}/carousel?_limit=4`);
+  }
+
+  delproduct(data: any): Observable<any> {
+    return this.http.delete(`${environment.backendUrl}/products/${data}`);
+  }
+
+  delcart(data: any): Observable<any> {
+    return this.http.delete(`${environment.backendUrl}/cart/${data}`);
+  }
+
+  delorders(data: any): Observable<any> {
+    return this.http.delete(`${environment.backendUrl}/order/${data}`);
+  }
+
+  delbuy(data: any): Observable<any> {
+    return this.http.delete(`${environment.backendUrl}/buy/${data}`);
+  }
+
+  getupdate(id: any): Observable<any> {
+    return this.http.get(`${environment.backendUrl}/products?id=${id}`);
+  }
+
+  addtocart(product: any): Observable<any> {
+    return this.http.post(`${environment.backendUrl}/cart`, product);
+  }
+
+  addtobuy(product: any): Observable<any> {
+    return this.http.post(`${environment.backendUrl}/buy`, product);
+  }
+
+  orderdetails(product: any): Observable<any> {
+    return this.http.post(`${environment.backendUrl}/order`, product);
+  }
+
+  updateproduct(data: any, id: any): Observable<any> {
+    return this.http.put(`${environment.backendUrl}/products/${id}`, data);
   }
 
   reloadSeller(): void {
     if (localStorage.getItem('seller')) {
       this.isSellerLoggedIn.next(true);
       this.router.navigate(['seller-home']);
-    }
-    else{
+    } else {
       this.router.navigate(['seller']);
     }
   }
+
   reloaduser(): void {
     if (localStorage.getItem('user')) {
       this.isUserLoggedIn.next(true);
       this.router.navigate(['']);
-    }
-    else{
+    } else {
       this.router.navigate(['user-login']);
     }
   }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { EnvironmentLoaderService } from 'ngx-env';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const loader = new EnvironmentLoaderService();
+
+// Load the environment variables before bootstrapping the application
+loader.loadEnvironment().then(() => {
+  bootstrapApplication(AppComponent, appConfig)
+    .catch((err) => console.error(err));
+}).catch((err:any) => {
+  console.error('Error loading environment variables:', err);
+});
