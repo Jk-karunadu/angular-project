430b21cfa8d1c7f1acdabfb0cea59c5968d4fd3d
Jashwanth-rit <158207370+Jashwanth-rit@users.noreply.github.com>
2024-12-13 18:48:46 +0530

    i think done

--- environment.prod.ts
+++ environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    backendUrl: process.env['BACKEND_URL'] || 'http://localhost:8080'
+    backendUrl: process.env['BACKEND_URL'] || 'https://e-commerce-8gne.onrender.com'
   };
 
   
