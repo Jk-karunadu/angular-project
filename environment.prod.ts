c9b16f33c5a4858f30a9df8198b4cd5bdc50baa9
Jashwanth-rit <158207370+Jashwanth-rit@users.noreply.github.com>
2024-12-13 22:45:28 +0530

    Update environment.prod.ts

--- environment.prod.ts
+++ environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    backendUrl: process.env['BACKEND_URL'] || 'https://e-commerce-8gne.onrender.com'
+    backendUrl: 'https://e-commerce-8gne.onrender.com'
   };
 
   
